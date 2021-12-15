9019b3e5a2ac747bf7a7b97c3638a43e81150e44
ErcagCan <ercag.can@student.uantwerpen.be>
2021-12-15 02:24:14 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TestSFML.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestSFML.dir/main.cpp.o"
+  "TestSFML"
+  "TestSFML.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestSFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
